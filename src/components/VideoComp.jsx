ba28b44ede7211e42a1e11f855189f36624490d1
Neeraj Hariyale <115395529+neerajhariyale@users.noreply.github.com>
2025-03-01 21:24:33 +0530

    Update VideoComp.jsx

--- src/components/VideoComp.jsx
+++ src/components/VideoComp.jsx
@@ -2,34 +2,31 @@ import React, { useEffect, useRef } from 'react'
 import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 import { useParams } from 'react-router-dom';
 
-
 const VideoComp = () => {
-
     const { roomID } = useParams();
-    console.log(roomID);
     const containerRef = useRef(null);
 
     useEffect(() => {
-        let myMeeting = async (element) => {
-
+        const myMeeting = async () => {
             if (!roomID || !containerRef.current) return;
-
 
             const appID = 2046966053;
             const serverSecret = "49045cc7498f82af2f99d3262070d5f5";
-            const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, Date.now().toString(), "Anonymous");
-            const zp = ZegoUIKitPrebuilt.create(kitToken);
+            const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+                appID,
+                serverSecret,
+                roomID,
+                Date.now().toString(),
+                "Anonymous"
+            );
 
+            const zp = ZegoUIKitPrebuilt.create(kitToken);
             zp.joinRoom({
-                container: element,
+                container: containerRef.current,
                 sharedLinks: [
                     {
                         name: 'Personal link',
-                        url:
-                            window.location.protocol + '//' +
-                            window.location.host + window.location.pathname +
-                            '?roomID=' +
-                            roomID,
+                        url: `${window.location.origin}/room/${roomID}`,
                     },
                 ],
                 scenario: {
@@ -37,6 +34,7 @@ const VideoComp = () => {
                 },
             });
         };
+
         myMeeting();
 
         return () => {
@@ -53,10 +51,6 @@ const VideoComp = () => {
             style={{ width: '100vw', height: '100vh' }}
         ></div>
     );
-
-
 };
 
-
-
-export default VideoComp
+export default VideoComp;
